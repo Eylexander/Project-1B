c685824a4a2d3c73a6fa2c36d83558df5bc4fcaa
Eylexander <eylexander88@gmail.com>
2021-06-03 10:58:42 +0200

    Package.json update
    Changelog :
    - Clean install with npm init
    - Added a todo list
    - Removed .gitattributes
    - Fixed clear command
    - Tried to fix cpu problem on hoststats

--- command/clear.js
+++ command/clear.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 
 module.exports = {
     name : "clear",
     description : "To clear chat",
     aliases : ['clean','delete'],
     async execute(client, message, args) {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("You need `manage_messages` permission to execute this command.");
-        if(!args[0]) return message.channel.send("You need to enter an amount.");
-        message.channel.bulkDelete((args[0])).then(() => {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("You need `manage_messages` permission to execute this command.").then(message => {setTimeout(() => {message.delete()}, 2500)});
+        if(!Number(args[0])) return message.channel.send("You need to enter a valid amount.");
+        message.channel.bulkDelete(Number(args[0]) + 1).then(() => {
             message.channel.send(`Cleared ${args[0]} messages.`)
                 .then(message => {
                     setTimeout(() => {
--- command/hoststats.js
+++ command/hoststats.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 var os = require('os');
+const client = new Discord.Client();
 const log = message => {console.log(`[${moment().format('MM-DD HH:mm:ss.SSS')}] ${message}`)};
 const moment = require('moment');
 
@@ -12,18 +13,18 @@ module.exports = {
         const freeram = ((os.freemem() / 10**6 + " ").split('.')[0]);
         const usedram = (((os.totalmem() - os.freemem()) / 10**6 + " ").split('.')[0]);
         const prctfreeram = (((os.freemem() * 100) / os.totalmem + " ").split('.')[0]);
-        // console.log(os.cpus());
+        // console.log(process.cpuUsage());
 
         const embed = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setTitle("Statistics")
-            // .setThumbnail(client.user.displayAvatarURL)
+            .setThumbnail(client.user.displayAvatarURL({ dynamic : true }))
             .setDescription("Stats of the host")
             .addFields(
               { name: 'Hostname', value: `${os.hostname}`, inline: true},
               { name: 'Uptime (s)', value: `${os.uptime}`, inline: true},
-            //   { name: 'CPU', value: `Percentage of CPU Usage: ${cpuUP}%`, inline: true }
-              { name: 'Memory (RAM)', value: `Total Memory: ${totalram}MB\nUsed Memory: ${usedram}MB\nFree Memory: ${freeram}MB\nPercentage Of Free Memory: ${prctfreeram}%`, inline: false},
+              { name: 'CPU', value: `Percentage of CPU Usage: ${os.cpus()}%`, inline: true },
+              { name: 'Memory (RAM)', value: `Total Memory: ${totalram}MB\nUsed Memory: ${usedram}MB\nFree Memory: ${freeram}MB\nPercentage of Free Memory: ${prctfreeram}%`, inline: false},
               { name: 'Language', value: 'Discord.js', inline: true},
             )
             .setTimestamp()
@@ -32,4 +33,3 @@ module.exports = {
         message.channel.send(embed)
     }
 };
-
