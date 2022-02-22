73434341a14adedf1f1f6a1ebfea74bbe9e4a3e0
Eylexander <eylexander88@gmail.com>
2022-02-22 23:06:09 +0100

    Weather Command
    Changelog :
    - Weather command without grammar issue

--- commands/Information/weather.js
+++ commands/Information/weather.js
@@ -1,13 +1,46 @@
 const Discord = require('discord.js');
+const axios = require('axios');
+const { weathertoken } = require('../../settings.json');
 
 module.exports.help = {
-    name : "ping",
-    description: 'Ping command',
-    aliases : ['pong'],
-    usage : 'none'
+    name : "weather",
+    description: 'Weather command',
+    aliases : ['wthr', 'meteo'],
+    usage : '[city]'
 };
 
 module.exports.execute = async (client, message, args) => {
-    const msg = await message.channel.send('Pinging Bot ...')
-    msg.edit(`Pong! Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms.`);
+    if (!args[0]) {
+        message.channel.send('Check your inputs, name the city you want to check please.')
+    } else {
+        const inputs = args[0].toLowerCase();
+        const FirstUpperCase = inputs.charAt(0).toUpperCase() + inputs.slice(1);
+
+        const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${FirstUpperCase}&units=metric&appid=${weathertoken}`)
+
+        const currentTemp = Math.ceil(data.main.temp)
+        const wind = data.wind.speed;
+        const icon = data.weather[0].icon
+        const cityName = FirstUpperCase
+        const country = data.sys.country
+        const cloudness = data.weather[0].description;
+        const { pressure, humidity, temp_max, temp_min  } = data.main;
+
+        const weatherEmbed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(`The temperature is ${currentTemp}\u00B0C in ${cityName}, ${country}`)
+            .addFields(
+                { name: `Maximum Temperature:`, value: `${temp_max}\u00B0C`, inline: true },
+                { name: `Minimum Temperature:`, value: `${temp_min}\u00B0C`, inline: true },
+                { name: `Humidity:`, value: `${humidity} %`, inline: true },
+                { name: `Wind Speed:`, value: `${wind} m/s`, inline: true },
+                { name: `Pressure:`, value: `${pressure} hpa`, inline: true },
+                { name: `Cloudiness:`, value: `${cloudness}`, inline: true },
+            )
+            .setThumbnail(`http://openweathermap.org/img/w/${icon}.png`)
+            .setTimestamp()
+            .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+            
+        message.channel.send(weatherEmbed);
+    }
 };
